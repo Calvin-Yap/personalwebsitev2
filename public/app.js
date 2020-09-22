e2159945f562e78b75f2704c469af327a67aa390
Calvin Yap <calvin.yap@ryerson.ca>
2020-09-21 23:10:54 -0400

    inital commit

---
+++ public/app.js
@@ -0,0 +1,14 @@
+
+
+const config = {
+    apiKey: "AIzaSyAamoScOTji1KddMOjPMTIrt1YLEWEJMqc",
+    authDomain: "webportfolio-1.firebaseapp.com",
+    databaseURL: "https://webportfolio-1.firebaseio.com",
+    projectId: "webportfolio-1",
+    storageBucket: "webportfolio-1.appspot.com",
+    messagingSenderId: "935937646461",
+    appId: "1:935937646461:web:b4cf6831368065b6563614"
+  };
+    firebase.initializeApp(config);
+
+
