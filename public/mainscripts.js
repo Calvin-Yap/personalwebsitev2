e7da7d82944fb32018615483cc2aff98503aafe1
Calvin Yap <calvin.yap@ryerson.ca>
2020-10-22 05:37:39 -0400

    image slider

--- public/mainscripts.js
+++ public/mainscripts.js
@@ -139,4 +139,30 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $('.next').on('click', function(){
+   
+    var currentImg=$('.active');
+    var nextImg=currentImg.next();
+    var firstImg=$('#first');
+    
+
+    if(nextImg.length){
+      currentImg.removeClass('active').css('z-index',-10);
+      nextImg.addClass('active').css('z-index',10);
+    }else{
+      currentImg.removeClass('active').css('z-index',-10);
+      nextImg = firstImg.addClass('active').css('z-index',10);
+    }
+  });
+  $('.prev').on('click', function(){
+    var currentImg=$('.active');
+    var prevImg=currentImg.prev();
+    if(prevImg.length){
+      currentImg.removeClass('active').css('z-index',-10);
+      prevImg.addClass('active').css('z-index',10);
+    }
+  });
+})
+
 
