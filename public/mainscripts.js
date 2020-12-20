b861249188b4dee5394cfff25a6ad4fcc45c155e
Calvin Yap <calvin.yap@ryerson.ca>
2020-12-20 14:47:49 -0500

    Completed Dec 20 update

--- public/mainscripts.js
+++ public/mainscripts.js
@@ -6,12 +6,6 @@ function myFunction() {
         x.className = "topnav";
       }
     }
-
-
-
-    
-
-
   $(window).scroll(function() {
     var y = $(this).scrollTop();
     if (y >= 76) {  $('#myTopnav').addClass('bottomlipshadow'); }
@@ -24,14 +18,68 @@ function myFunction() {
       $('#myTopnav').addClass('bottomlipshadow');
     }, 300));
 });
-  
-$(document).ready(function(){$(".typerjs").hide().fadeIn(2000);});
 $(document).ready(function(){
-    var textWrapper = document.querySelector('.main .letters');
+  /*var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);*/
+  if ($(window).width() < 1201) {
+  jQuery(".extraproj .projectbox .overlay1").fitText(1.5,{ minFontSize: '0px', maxFontSize: '150px' });
+
+}
+else{
+  jQuery(".extraproj .projectbox .overlay1").fitText(1.8,{ minFontSize: '0px', maxFontSize: '200px' });
+}
+$("#projects").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#projectscroll").offset().top - $('.topnav').outerHeight(true)
+  }, 1000);
+});
+$("#home").click(function() {
+  $('html, body').animate({
+      scrollTop:0}, 1000);
+});
+$("#about").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#aboutscroll").offset().top - $('.topnav').outerHeight(true)
+  }, 1000);
+});
+const buffer = 800;
+const projectsCheckPoint = $("#projectscroll").offset().top - $('.topnav').outerHeight(true) - buffer;
+const extraProjectsCheckPoint = $("#extraProjects").offset().top - $('.topnav').outerHeight(true) - buffer;
+const aboutMeCheckPoint =$('#aboutscroll').offset().top - $('.topnav').outerHeight(true) - buffer;
+document.querySelector("#mainprojects").style.opacity = 0;
+document.querySelector(".extraproj").style.opacity = 0;
+document.querySelector(".aboutsection").style.opacity = 0;
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() >= projectsCheckPoint ) {
+    $('#mainprojects').animate({opacity: 1}, 1000);
+    if ($(this).scrollTop() >= extraProjectsCheckPoint ) {
+      $('.extraproj').animate({opacity: 1}, 1500);
+    } 
+    if ($(this).scrollTop() >= aboutMeCheckPoint ) {
+      $('.aboutsection').animate({opacity: 1}, 1500);
+    } 
+  } 
+});
+  
+
+  
+  var textWrapper = document.querySelector('.main .letters');
     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
     var animation = anime.timeline({loop: false})
   .add({
+    begin: function(){
+      document.querySelector('#bigText').style.display = 'block';
+    },
     targets: '.main .letter',
     scale: [0.3,1],
     opacity: [0,1],
@@ -72,6 +120,25 @@ $(document).ready(function(){
     easing: 'linear'
   }).add({
     targets: '.polyMorph',
+    begin: function(){
+      document.querySelector('#bigText').style.display = 'none';
+      if($(window).width() > 1202){
+        document.querySelector('#biomove').style.marginTop = "25vh";
+      }
+      else if($(window).width() <1201 && $(window).width() >994){
+        document.querySelector('#biomove').style.marginTop = "25vh";
+      }
+      else if($(window).width() <923 && $(window).width() >770){
+        document.querySelector('#biomove').style.marginTop = "25vh";
+       }
+      else if($(window).width() <769 && $(window).width() >578){
+        document.querySelector('#biomove').style.marginTop = "25vh";
+      }
+      else{
+        document.querySelector('#biomove').style.marginTop = "30vh";
+
+      }
+    },
     points: [
       { value: '1920.5,1080 0.5,1080 0.5,0 373.4,0 523,774' },
       { value: '1920.5,1080 0.5,1080 0.5,0 0,1 523,774' }
@@ -85,6 +152,7 @@ $(document).ready(function(){
     opacity: [0,1],
     duration: 500
   });
+  
   document.querySelector('#restart').onclick = animation.restart;
 
 });
@@ -98,9 +166,18 @@ $(document).ready(function(){
     
 
     if(nextImg.length){
+      if(nextImg.attr("id") == "second"){
+        $('#aboutImageHeading').text("10 Year Plaque in Karate");
+      }else if(nextImg.attr("id") == "thrid"){
+        $('#aboutImageHeading').text("At the Museum of Illusions");
+      }else if(nextImg.attr("id") == "last"){
+        $('#aboutImageHeading').text("Photo with the RAM");
+      }
+
       currentImg.removeClass('active').css('z-index',-10);
       nextImg.addClass('active').css('z-index',10);
     }else{
+      $('#aboutImageHeading').text("My Trip to LA");
       currentImg.removeClass('active').css('z-index',-10);
       nextImg = firstImg.addClass('active').css('z-index',10);
     }
@@ -109,6 +186,15 @@ $(document).ready(function(){
     var currentImg=$('.active');
     var prevImg=currentImg.prev();
     if(prevImg.length){
+      if(prevImg.attr("id") == "first"){
+        $('#aboutImageHeading').text("My Trip to LA");
+      }else if(prevImg.attr("id") == "second"){
+        $('#aboutImageHeading').text("10 Year Plaque in Karate");
+      }else if(prevImg.attr("id") == "thrid"){
+        $('#aboutImageHeading').text("At the Museum of Illusions");
+      }else if(prevImg.attr("id") == "last"){
+        $('#aboutImageHeading').text("Photo with the RAM");
+      }
       currentImg.removeClass('active').css('z-index',-10);
       prevImg.addClass('active').css('z-index',10);
     }
@@ -126,6 +212,7 @@ $(document).ready(function(){
 
         counter++;
         service.querySelector('i').style.display = 'none';
+        service.querySelector('.overlay1').style.display='inline';
 
         anime({
           targets: service.querySelector('.arrow'),
@@ -136,6 +223,7 @@ $(document).ready(function(){
       }else{
         counter++;
         service.querySelector('i').style.display = 'inline';
+        service.querySelector('.overlay1').style.display='none';
         anime({
           targets: service.querySelector('.arrow'),
           easing: 'easeOutExpo',
--- public/writeup.js
+++
@@ -1,6 +0,0 @@
-var extra1 = "Java - Grocery Cart";
-
-$('#one').text('Hi I am replace');
-
-
-var intervalimg = "/assets/IntervalUnion.jpg"
